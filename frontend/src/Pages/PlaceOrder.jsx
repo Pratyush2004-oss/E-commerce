bca2bb21d85910f6d019981c974dff1bed4e0fd6
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-11-20 23:51:22 +0530

    Change

--- frontend/src/Pages/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder.jsx
@@ -12,24 +12,24 @@ const PlaceOrder = () => {
     <div className='flex flex-col sm:flex-row justify-between gap-5 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       {/* Left Side */}
       <div className='flex flex-col gap-4 w-full sm:w-[480px]'>
-        <div className='text-xl sm:text-2xl my-3'>
+        <div className='my-3 text-xl sm:text-2xl '>
           <Title text1={'DELIVERY'} text2={'INFORMATION'} />
         </div>
         <div className='flex gap-3'>
-          <input type='text' className='input w-full max-md:input-sm input-bordered' placeholder='First Name' />
-          <input type='text' className='input w-full max-md:input-sm input-bordered' placeholder='Last Name' />
+          <input type='text' className='w-full input max-md:input-sm input-bordered' placeholder='First Name' />
+          <input type='text' className='w-full input max-md:input-sm input-bordered' placeholder='Last Name' />
         </div>
-        <input type='email' className='input w-full max-md:input-sm input-bordered' placeholder='Email Address' />
-        <input type='text' className='input w-full max-md:input-sm input-bordered' placeholder='Street' />
+        <input type='email' className='w-full input max-md:input-sm input-bordered' placeholder='Email Address' />
+        <input type='text' className='w-full input max-md:input-sm input-bordered' placeholder='Street' />
         <div className='flex gap-3'>
-          <input type='text' className='input w-full max-md:input-sm input-bordered' placeholder='City' />
-          <input type='text' className='input w-full max-md:input-sm input-bordered' placeholder='State' />
+          <input type='text' className='w-full input max-md:input-sm input-bordered' placeholder='City' />
+          <input type='text' className='w-full input max-md:input-sm input-bordered' placeholder='State' />
         </div>
         <div className='flex gap-3'>
-          <input type='number' className='input w-full max-md:input-sm input-bordered' placeholder='ZIP Code' />
-          <input type='text' className='input w-full max-md:input-sm input-bordered' placeholder='Country' />
+          <input type='number' className='w-full input max-md:input-sm input-bordered' placeholder='ZIP Code' />
+          <input type='text' className='w-full input max-md:input-sm input-bordered' placeholder='Country' />
         </div>
-        <input type='number' className='input w-full max-md:input-sm input-bordered' placeholder='Phone Number' />
+        <input type='number' className='w-full input max-md:input-sm input-bordered' placeholder='Phone Number' />
       </div>
 
       {/* ----------------------------Right Side-------------------------- */}
@@ -40,25 +40,25 @@ const PlaceOrder = () => {
         <div className='mt-12 '>
           <Title text1={'PAYMENT'} text2={'METHOD'} />
           {/* ------------------Payment Method Selection---------------- */}
-          <div className='flex gap-3 flex-col lg:flex-row'>
-            <div onClick={() => setMethod('stripe')} className='items-center flex gap-3 border p-2 px-3 cursor-pointer'>
+          <div className='flex flex-col gap-3 lg:flex-row'>
+            <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 p-2 px-3 border cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-500' : ''}`}></p>
               <img src={assets.stripe_logo} alt='stripe' className='h-5 mx-4' />
             </div>
-            <div onClick={() => setMethod('razor-Pay')} className='items-center flex gap-3 border p-2 px-3 cursor-pointer'>
+            <div onClick={() => setMethod('razor-Pay')} className='flex items-center gap-3 p-2 px-3 border cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razor-Pay' ? 'bg-green-500' : ''}`}></p>
               <img src={assets.razorpay_logo} alt='razorPay' className='h-5 mx-4' />
             </div>
-            <div onClick={() => setMethod('cod')} className='items-center flex gap-3 border p-2 px-3 cursor-pointer'>
+            <div onClick={() => setMethod('cod')} className='flex items-center gap-3 p-2 px-3 border cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-500' : ''}`}></p>
-              <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+              <p className='mx-4 text-sm font-medium text-gray-500'>CASH ON DELIVERY</p>
             </div>
           </div>
-          <div className='w-full text-end mt-8'>
+          <div className='w-full mt-8 text-end'>
             <button onClick={() => {
               toast.success('Order placed')
               navigate('/orders')
-            }} className='bg-black text-white px-16 py-3 text-sm active:bg-gray-700'>PLACE YOUR ORDER</button>
+            }} className='px-16 py-3 text-sm text-white bg-black active:bg-gray-700'>PLACE YOUR ORDER</button>
           </div>
         </div>
       </div>
